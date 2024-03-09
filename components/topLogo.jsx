d9c9639e3f8d0c04f3c466dabe1ba1bf22a20694
joyjsmun <joy.js.mun@gmail.com>
2024-03-09 18:04:34 +0900

    ADD: layout css

--- components/topLogo.jsx
+++ components/topLogo.jsx
@@ -4,7 +4,7 @@ import LogoImage from "../public/assets/images/logo.png";
 
 const TopLogo = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 bg-white p-4 flex items-center justify-center">
+    <div className="fixed top-0 left-0 right-0 z-50 bg-white px-4 py-3 flex items-center justify-center">
       <div className="flex flex-row items-center justify-center ">
         <Image src={LogoImage} alt="Logo" className="w-6 h-7" />
         <span className="text-orange-500 text-base font-blackHans ml-1 mt-2">
