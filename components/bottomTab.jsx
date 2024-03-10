d5f90bbe1dc64c63b3ccb79be10778ab55b5d0a3
joyjsmun <joy.js.mun@gmail.com>
2024-03-10 21:51:40 +0900

    ADD: bottom tab css

--- components/bottomTab.jsx
+++ components/bottomTab.jsx
@@ -22,37 +22,37 @@ const BottomTab = ({ location }) => {
       <div className=" bg-[#231f20] px-4 py-3 flex flex-around justify-around">
         <div onClick={() => router.push("/home")}>
           {location === "home" ? (
-            <Image src={home1} className="w-11 h-11" alt="Home Active" />
+            <Image src={home1} className="w-10 h-10" alt="Home Active" />
           ) : (
             <Image src={home} className="w-10 h-10" alt="Home" />
           )}
         </div>
         <div onClick={() => router.push("/hangout")}>
           {location === "hangout" ? (
-            <Image src={search1} className="w-11 h-11" alt="Hangout Active" />
+            <Image src={search1} className="w-10 h-10" alt="Hangout Active" />
           ) : (
             <Image src={search} className="w-10 h-10" alt="Hangout" />
           )}
         </div>
         <div onClick={() => router.push("/walkRequestInfo")}>
           {location === "walkRequestInfo" ? (
-            <Image src={walk2} className="w-11 h-11" alt="Walk Active" />
+            <Image src={walk2} className="w-10 h-10" alt="Walk Active" />
           ) : (
             <Image src={walk} className="w-10 h-10" alt="Walk" />
           )}
         </div>
         <div onClick={() => router.push("/supportRequestInfo")}>
           {location === "supportRequestInfo" ? (
-            <Image src={hand1} className="w-11 h-11" alt="Support Active" />
+            <Image src={hand1} className="w-10 h-10" alt="Support Active" />
           ) : (
             <Image src={hand} className="w-10 h-10" alt="Support" />
           )}
         </div>
         <div onClick={() => router.push("/profile")}>
           {location === "profile" ? (
-            <Image src={profile1} className="w-11 h-11" alt="Profile Active" />
+            <Image src={profile1} className="w-10 h-10" alt="Profile Active" />
           ) : (
-            <Image src={profile} className="w-11 h-11" alt="Profile" />
+            <Image src={profile} className="w-10 h-10" alt="Profile" />
           )}
         </div>
       </div>
