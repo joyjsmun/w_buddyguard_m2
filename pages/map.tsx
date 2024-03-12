09547e8a757e83ee2aaeecefe382ff2fbf050066
joyjsmun <joy.js.mun@gmail.com>
2024-03-13 02:59:06 +0900

    ADD: location db setting

--- pages/map.tsx
+++ pages/map.tsx
@@ -6,10 +6,26 @@ import {
   useJsApiLoader,
 } from "@react-google-maps/api";
 import Layout from "@/components/layout";
+import initializeFirebaseClient from "../lib/initFirebase";
+import { getAuth } from "firebase/auth";
+import { collection, doc, setDoc, onSnapshot } from "firebase/firestore";
+
+interface UserLocation {
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
+const { db: firestore } = initializeFirebaseClient();
 
 const Map = () => {
   const containerStyle = { width: "100%", height: "100vh" };
   const [currentLocation, setCurrentLocation] = useState({ lat: 0, lng: 0 });
+  const [userLocations, setUserLocations] = useState<
+    Record<string, UserLocation>
+  >({});
+
   const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY || "";
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
@@ -35,10 +51,20 @@ const Map = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          setCurrentLocation({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          });
+          const { latitude, longitude } = position.coords;
+          setCurrentLocation({ lat: latitude, lng: longitude });
+
+          // Update the user's location in Firestore
+          const auth = getAuth();
+          const userId = auth.currentUser?.uid;
+          if (userId) {
+            const userRef = doc(firestore, "users", userId);
+            setDoc(
+              userRef,
+              { location: { latitude, longitude } },
+              { merge: true }
+            );
+          }
         },
         () => {
           console.error("Error getting location");
@@ -51,6 +77,23 @@ const Map = () => {
 
   useEffect(() => {
     getLocation();
+
+    // Listen for changes in user locations
+    const unsubscribe = onSnapshot(
+      collection(firestore, "users"),
+      (snapshot) => {
+        const locations = snapshot.docs.reduce((acc, doc) => {
+          acc[doc.id] = doc.data() as UserLocation;
+          return acc;
+        }, {} as Record<string, UserLocation>);
+        setUserLocations(locations);
+      }
+    );
+
+    return () => {
+      // Clean up the listener when the component unmounts
+      unsubscribe();
+    };
   }, []);
 
   return (
